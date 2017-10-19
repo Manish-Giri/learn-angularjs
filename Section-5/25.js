d36e24a1ff3c0fcb1a1e9efbda47f9bc0c5ba2de
Manish Giri <manish.giri.me@gmail.com>
2017-10-18 22:39:28 -0400

    Add current changes

--- Section-5/25.js
+++ Section-5/25.js
@@ -4,6 +4,12 @@ var angularApp = angular.module('angularApp', []);
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function($scope) {
     console.log($scope);
+
+    $scope.name = "Jane Doe";
+    $scope.getName = function() { return "Hi, " + this.name};
+
+
+
     
 
 
